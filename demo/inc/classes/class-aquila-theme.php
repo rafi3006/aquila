bc071bdd9212b6836d4b33a7acbd4523f804216a
Rafiu Bin Wahed <rafiuwahed@gmail.com>
2021-02-01 23:04:18 +0600

    01/feb/21

---
+++ demo/inc/classes/class-aquila-theme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Bootstrap the Theme.
+ * 
+ * @package Aquila
+ */
+
+ namespace AQUILA_THEME\Inc;
+
+use Aquila_Theme\Inc\Traits\Singleton;
+
+class AQUILA_THEME {
+     use Singleton;
+ }
+?>
