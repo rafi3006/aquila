7b83308738db20a49e38eea73bb6807e951711a4
Rafiu Bin Wahed <rafiuwahed@gmail.com>
2021-02-01 14:35:53 +0600

    updated aquila theme 01/Feb/21

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
+
 </head>
-<body>
+<body <?php body_class(); ?>>
+
+<?php 
+if (function_exists ('wp_body_open') ) {
+    wp_body_open();
+}
+?>
+
     <header>Header</header>
